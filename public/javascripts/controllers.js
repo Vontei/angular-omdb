9c95081e8c9529037bf202f1a10870be9cd6e22f
Nick Mattei <nmattei89@gmail.com>
2015-08-30 12:46:04 -0600

    Changed to inline array annotation

--- public/javascripts/controllers.js
+++ public/javascripts/controllers.js
@@ -1,28 +1,31 @@
-app.controller('homeController', function($scope, $http){
+app.controller('homeController', ['$scope','$http',
+  function($scope, $http){
   $scope.getMovie = function(){
     $http.get('http://www.omdbapi.com/?s='+ $scope.search).then(function (data) {
       console.log(data)
       $scope.results = data.data.Search
     })
   }
-});
+}]);
 
 
-app.controller('movieController',function ($scope, $http) {
-  $scope.message = "Welcome!"
-  $scope.getMovie = function(){
-    $http.get('http://www.omdbapi.com/?s='+ $scope.search).then(function (data) {
-      console.log(data)
-      $scope.results = data.data.Search
-    })
-  }
-})
+app.controller('movieController', ["$scope","$http",
+  function ($scope, $http) {
+    $scope.message = "Welcome!"
+    $scope.getMovie = function(){
+      $http.get('http://www.omdbapi.com/?s='+ $scope.search).then(function (data) {
+        console.log(data)
+        $scope.results = data.data.Search
+      })
+    }
+}])
 
 
-app.controller('filmController', function($scope, $http, $routeParams){
+app.controller('filmController', ["$scope",'$http','$routeParams',
+  function($scope, $http, $routeParams){
   console.log('im here')
   $http.get('http://www.omdbapi.com/?i=' + $routeParams.id).then(function (data) {
     console.log(data)
     $scope.movieResults = data.data
   })
-})
+}])
